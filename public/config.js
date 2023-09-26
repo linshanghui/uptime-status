dce558d006ce73ddc919c9f5381d536a034f2901
阿宅 <39202956+linshanghui@users.noreply.github.com>
2023-09-26 12:11:07 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -9,7 +9,7 @@ window.Config = {
     'm784488775-dd1ad84b209c05f8e185c33e',
     'm784490063-7b5da437e7f1e0d67613714d',
     'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1881788-219fff7b6d4f3137a89f1962p',
   ],
 
   // 日志天数
@@ -29,8 +29,8 @@ window.Config = {
       url: 'https://github.com/yb/uptime-status'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: '陪伴Ta「博客」',
+      url: 'http://pbt.icu/'
     },
   ],
 };
